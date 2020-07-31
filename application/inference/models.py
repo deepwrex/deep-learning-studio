a5c312b326debb3677195bc51b55889d1b851dd8
soumik12345 <19soumik.rakshit96@gmail.com>
2020-07-31 12:47:31 +0530

    Added Inception ResNet V2

--- application/inference/models.py
+++ application/inference/models.py
@@ -2,56 +2,63 @@ import tensorflow as tf
 
 
 MODEL_DICT = {
-            'VGG16': [
-                tf.keras.applications.vgg16.VGG16,
-                tf.keras.applications.vgg16.preprocess_input,
-                tf.keras.applications.vgg16.decode_predictions,
-                'block5_conv3', (224, 224),
-                ['block5_pool', 'flatten', 'fc1', 'fc2', 'predictions']
-            ],
-            'VGG19': [
-                tf.keras.applications.vgg19.VGG19,
-                tf.keras.applications.vgg19.preprocess_input,
-                tf.keras.applications.vgg19.decode_predictions,
-                'block5_conv4', (224, 224),
-                ['block5_pool', 'flatten', 'fc1', 'fc2', 'predictions']
-            ],
-            'ResNet50': [
-                tf.keras.applications.resnet50.ResNet50,
-                tf.keras.applications.resnet50.preprocess_input,
-                tf.keras.applications.resnet50.decode_predictions,
-                'conv5_block3_out', (224, 224),
-                ['avg_pool', 'predictions']
-            ],
-            'ResNet101': [
-                tf.keras.applications.resnet.ResNet101,
-                tf.keras.applications.resnet.preprocess_input,
-                tf.keras.applications.resnet.decode_predictions,
-                'conv5_block3_out', (224, 224),
-                ['avg_pool', 'predictions']
-            ],
-            'ResNet152': [
-                tf.keras.applications.resnet.ResNet152,
-                tf.keras.applications.resnet.preprocess_input,
-                tf.keras.applications.resnet.decode_predictions,
-                'conv5_block3_out', (224, 224),
-                ['avg_pool', 'predictions']
-            ],
-            'MobileNetV1': [
-                tf.keras.applications.mobilenet.MobileNet,
-                tf.keras.applications.mobilenet.preprocess_input,
-                tf.keras.applications.mobilenet.decode_predictions,
-                'conv_pw_13_relu', (224, 224),
-                [
-                    'global_average_pooling2d', 'reshape_1', 'dropout',
-                    'conv_preds', 'reshape_2', 'predictions'
-                ]
-            ],
-            'MobileNetV2': [
-                tf.keras.applications.mobilenet_v2.MobileNetV2,
-                tf.keras.applications.mobilenet_v2.preprocess_input,
-                tf.keras.applications.mobilenet_v2.decode_predictions,
-                'out_relu', (224, 224),
-                ['global_average_pooling2d', 'predictions']
-            ]
-        }
+    'VGG16': [
+        tf.keras.applications.vgg16.VGG16,
+        tf.keras.applications.vgg16.preprocess_input,
+        tf.keras.applications.vgg16.decode_predictions,
+        'block5_conv3', (224, 224),
+        ['block5_pool', 'flatten', 'fc1', 'fc2', 'predictions']
+    ],
+    'VGG19': [
+        tf.keras.applications.vgg19.VGG19,
+        tf.keras.applications.vgg19.preprocess_input,
+        tf.keras.applications.vgg19.decode_predictions,
+        'block5_conv4', (224, 224),
+        ['block5_pool', 'flatten', 'fc1', 'fc2', 'predictions']
+    ],
+    'ResNet50': [
+        tf.keras.applications.resnet50.ResNet50,
+        tf.keras.applications.resnet50.preprocess_input,
+        tf.keras.applications.resnet50.decode_predictions,
+        'conv5_block3_out', (224, 224),
+        ['avg_pool', 'predictions']
+    ],
+    'ResNet101': [
+        tf.keras.applications.resnet.ResNet101,
+        tf.keras.applications.resnet.preprocess_input,
+        tf.keras.applications.resnet.decode_predictions,
+        'conv5_block3_out', (224, 224),
+        ['avg_pool', 'predictions']
+    ],
+    'ResNet152': [
+        tf.keras.applications.resnet.ResNet152,
+        tf.keras.applications.resnet.preprocess_input,
+        tf.keras.applications.resnet.decode_predictions,
+        'conv5_block3_out', (224, 224),
+        ['avg_pool', 'predictions']
+    ],
+    'MobileNetV1': [
+        tf.keras.applications.mobilenet.MobileNet,
+        tf.keras.applications.mobilenet.preprocess_input,
+        tf.keras.applications.mobilenet.decode_predictions,
+        'conv_pw_13_relu', (224, 224),
+        [
+            'global_average_pooling2d', 'reshape_1', 'dropout',
+            'conv_preds', 'reshape_2', 'predictions'
+        ]
+    ],
+    'MobileNetV2': [
+        tf.keras.applications.mobilenet_v2.MobileNetV2,
+        tf.keras.applications.mobilenet_v2.preprocess_input,
+        tf.keras.applications.mobilenet_v2.decode_predictions,
+        'out_relu', (224, 224),
+        ['global_average_pooling2d', 'predictions']
+    ],
+    'Inception-ResNet-V2': [
+        tf.keras.applications.inception_resnet_v2.InceptionResNetV2,
+        tf.keras.applications.inception_resnet_v2.preprocess_input,
+        tf.keras.applications.inception_resnet_v2.decode_predictions,
+        'conv_7b_ac', (224, 224),
+        ['avg_pool', 'predictions']
+    ]
+}
